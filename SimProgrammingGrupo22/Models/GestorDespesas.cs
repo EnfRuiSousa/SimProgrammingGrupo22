b2d6499f338c7a8302575e94761f71b4c7b5d949
Cristina Teixeira <120024738+criscaldeirat@users.noreply.github.com>
2026-04-09 20:39:21 +0100

    Adição do ficheiro GestorDespesas.cs

---
+++ SimProgrammingGrupo22/Models/GestorDespesas.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimProgrammingGrupo22.Models;
+
+namespace SimProgrammingGrupo22.Models
+{
+    internal class GestorDespesas
+    {
+        // --- METODOS CONTROLLER ---
+
+        /* adicionarDespesa(despesa) 
+         * <- DespesasObtidas (?)
+         * -> mensagem de sucesso ou erro
+         * */
+
+        /* obterTodasDespesas() 
+         * <- preciso buscar ao json
+         * -> ListaDespesas
+         * */
+
+        /* obterDespesasPorCategoria(categoria) 
+         * <- preciso buscar ao json filtrando por categoria
+         * -> ListaDespesasCategoria
+         */
+
+        /* calcularTotal() 
+         * <- preciso buscar ao json e somar os valores
+         * -> total
+         */
+
+        
+
+        
+
+    }
+
+    // -- METODOS VIEW ---
+
+    /* ListaDespesaAtualizada() 
+     * <- preciso buscar ao json
+     * -> ListaDespesas
+     */
+
+    /* DadosParaApresentacao() 
+     * <- preciso buscar ao json e formatar os dados para apresentação
+     * -> ListaDespesas
+     */
+
+    /* TotalAtualizado() 
+     * <- preciso buscar ao json e calcular o total
+     * -> total
+     */
+
+
+
+    class Despesa
+    {
+        // descricao
+        // valor
+        // categoria
+        // data
+    }
+
+    class CategoriaDespesa
+    {
+        // Alimentação
+        // Transporte
+        // Lazer
+        // Saúde
+    }
+
+   class jsonRepository
+    {
+        /* LerDespesas() 
+         * <- preciso ler o json
+         * -> ListaDespesas
+         */
+
+        /* GuardarDespesas() 
+         * <- preciso guardar a lista de despesas no json
+         * -> DadosAtualizados / mensagem de sucesso ou erro(?)
+         */
+
+    }
+
+}
