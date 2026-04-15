cd7d6609a4523e64c92781d490f4829bdfdbee77
RodrigoDuarte2 <rodrigolopesduarte1@gmail.com>
2026-04-15 21:04:28 +0100

    Controller Atualizado com Eventos
    Retirei o switch com a escolha do utilizador e substitui com eventos e handlers para ligar os eventos da View ao comportamento da aplicação.

--- SimProgrammingGrupo22/Program.cs
+++ SimProgrammingGrupo22/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public static void Main()
+  {
+      var gestor = new GestorDespesas();
+      var view = new ConsoleView();
+      var controller = new DespesaController(gestor, view);
+      controller.Iniciar();
+  }
